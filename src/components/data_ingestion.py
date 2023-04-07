f432c4924382b6d31ec446db3ed1e4311d71e7ac
pbansode121code <pritibansode@gmail.com>
2023-04-07 15:45:01 +0530

    add model trainer code

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,7 +6,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformationConfig 
+
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 
 
@@ -39,9 +44,11 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
-if __name__== "__main__":
+if __name__=="__main__":
     obj = DataIngestion()
     train_data, test_data, = obj.iniatiate_data_ingestion()
     print(train_data,test_data)
     data_transform = DataTransformation()
-    data_transform.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transform.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -94,4 +94,3 @@ class DataTransformation:
             )
         except Exception as e:
             raise CustomException(e,sys)
-
