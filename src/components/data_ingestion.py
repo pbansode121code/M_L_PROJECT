79aea03eac630d676993061504d875beaba4b856
pbansode121code <pritibansode@gmail.com>
2023-04-11 14:50:28 +0530

    push the ui part and model trainer file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,19 +1,17 @@
-import sys
 import os
+import sys
+#import sys
+#sys.path.append('src/package1')
 from src.exception import CustomException
 from src.logger import logging
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig 
-
+from src.components.data_transformation import DataTransformationConfig
 
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
-
-
-
 
 @dataclass
 class DataIngestionConfig:
@@ -23,27 +21,27 @@ class DataIngestionConfig:
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfig
+        self.ingestion_config = DataIngestionConfig()
     def iniatiate_data_ingestion(self):
         logging.info("Data Ingestion Started!!")
         try:
-            df = pd. read_csv("E:\stud.csv")
+            df = pd.read_csv('F:/class/ML/notebook/data/stud.csv')
             logging.info("read the dataset as dataframe")
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok = True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-            train_set,test_set = train_test_split(df,test_size=0.2,random_state=44)
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path,index= False,header=True)
-            logging.info("train test  split is done and saved the dfs")
-            logging.info("Data ingestion completed !!")
+            train_set, test_set = train_test_split(df,test_size=0.2,random_state=44)
+            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False,header=True)
+            logging.info("train test split is done and saved the dfs")
+            logging.info("Data ingestion completed!!")
 
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
+        
 if __name__=="__main__":
     obj = DataIngestion()
     train_data, test_data, = obj.iniatiate_data_ingestion()
